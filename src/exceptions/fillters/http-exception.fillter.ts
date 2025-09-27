55ac15d2b95bdbfce0b9cef4aef622b4d20f45f7
annalara123 <112590453+annalara123@users.noreply.github.com>
2025-09-27 18:47:37 -0300

    feat: adiciona filtro de exceções Http

---
+++ src/exceptions/fillters/http-exception.fillter.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { Response } from 'express';
+
+@Catch()
+export class HttpExceptionFillter implements ExceptionFilter{
+    catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Erro interno no servidor';
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
