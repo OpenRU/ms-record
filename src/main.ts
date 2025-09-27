170531a7ea1778a96515069babee6a385cfb3068
annalara123 <112590453+annalara123@users.noreply.github.com>
2025-09-27 15:04:15 -0300

    chore: adiciona class-validator às dependências

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }))
 }
 bootstrap();
