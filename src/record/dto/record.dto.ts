93cbc3bb5a75e0184d5c6dae2fc801c8dba713fa
annalara123 <112590453+annalara123@users.noreply.github.com>
2025-09-27 15:39:22 -0300

    feat: adiciona DTO

---
+++ src/record/dto/record.dto.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { IsDateString } from "class-validator";
+import { Period } from "generated/client";
+export class RecordDto{
+    readonly id?: number;
+    readonly user_id: string;
+    readonly menu_id: string;
+
+    @IsDateString()
+    readonly timestamp: string;
+    readonly period: Period;
+}
