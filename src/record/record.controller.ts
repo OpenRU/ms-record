6f1857120a56c6af9dea2c07f8427405d086820f
annalara123 <112590453+annalara123@users.noreply.github.com>
2025-09-28 22:53:50 -0300

    feat: adiciona a camada record.controller

--- src/record/record.controller.ts
+++ src/record/record.controller.ts
@@ -1,5 +1,46 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
+import { RecordService } from './record.service';
+import { RecordDto } from './dto/record.dto';
+import { UpdateRecordDto } from './dto/update-record.dto';
 
 @Controller('record')
-export class RecordController {}
+export class RecordController {
+  constructor(private readonly recordService: RecordService) {}
+
+  @Post()
+  async createRecord(@Body() recordDto: RecordDto) {
+    return this.recordService.create(recordDto);
+  }
+  @Get()
+  async showAllRecords(){
+    return this.recordService.showAllRecords()
+  }
+
+   @Get('history')
+  async history(@Query('user_id') userId: string) {
+    return this.recordService.getHistory(userId);
+  }
+
+  @Get('total')
+  async totalByDate(@Query('data') data: string) {
+    return this.recordService.getTotalMeals(data);
+  }
+
+  @Get(':id')
+  async getRecord(@Param('id', ParseIntPipe) id: number){
+    return this.recordService.getRecord(id);
+  }
+
+
+  @Put(':id')
+  async updateMeal(@Param('id', ParseIntPipe) id: number, @Body()updateRecordDto: UpdateRecordDto){
+    return this.recordService.updateRecord(id, updateRecordDto);
+  }
+
+ @Delete(':id')
+  async deleteRecord(@Param('id', ParseIntPipe) id: number){
+    return this.recordService.deleteRecord(id);
+  } 
+
+}
