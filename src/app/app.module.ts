4f9db8cc5810844bc423208afd1a04726811ba87
annalara123 <112590453+annalara123@users.noreply.github.com>
2025-09-28 22:48:13 -0300

    chore: adiciona imports e providers no modulo de app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecordModule } from 'src/record/record.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, RecordModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
